ac95c3112503e5a8b6144cea62b9fadeb69abb76
Ben Lamb O'Sullivan <64701953+blosmusic@users.noreply.github.com>
2022-06-18 18:05:40 +0100

    added looping for two beats

--- toneApp.js
+++ toneApp.js
@@ -1,11 +1,17 @@
 const synth = new Tone.PolySynth(Tone.Synth).toDestination();
 const now = Tone.now();
+const time = 1;
+
+//
+document.querySelector("button")?.addEventListener("click", async () => {
+  await Tone.start();
+  console.log("audio is ready");
+});
 
 const clock = new Tone.Clock((time) => {
   console.log(time);
-  synth.triggerAttackRelease("a4", "16n");
+  synth.triggerAttackRelease("f3", "16n");
 }, 1);
-
 
 let playIO = () => {
   console.log("button pressed");
@@ -18,4 +24,24 @@ let playIO = () => {
     document.getElementById("onoff").value = "Off";
     clock.stop();
   }
-};
+}
+
+const note1 = new Tone.Loop(time => {
+  synth.triggerAttackRelease("e3", "8n", time);
+}, "4n").start(0);
+const note2 = new Tone.Loop((time) => {
+  synth.triggerAttackRelease("a3", "8n", time);
+}, "4n").start("8n");
+
+let loopIO = () => {
+  console.log("loop pressed");
+
+  currentvalue = document.getElementById("loopButton").value;
+  if (currentvalue == "Off") {
+    document.getElementById("loopButton").value = "On";
+    Tone.Transport.start();
+  } else {
+    document.getElementById("loopButton").value = "Off";
+    Tone.Transport.stop();
+  }
+}
