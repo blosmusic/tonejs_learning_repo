04b97ec04a548757f5d5960cc15f4f4ad20a52f4
Ben Lamb O'Sullivan <64701953+blosmusic@users.noreply.github.com>
2022-07-01 17:11:46 +0100

    tested sequence option

--- toneApp.js
+++ toneApp.js
@@ -28,24 +28,54 @@ let playIO = () => {
 }
 
 //create a loop with two notes and set bpm
-Tone.Transport.bpm.value = 80;
+Tone.Transport.bpm.value = 60;
 
-const note1 = new Tone.Loop(time => {
-  synth.triggerAttackRelease("e2", "8n", time);
-}, "2n").start(0);
-const note2 = new Tone.Loop((time) => {
-  synth.triggerAttackRelease("a2", "8n", time);
-}, "2n").start("4n");
+// const note1 = new Tone.Loop((time) => {
+//   synth.triggerAttackRelease("e2", "8n", time);
+// }, "2n").start(0);
+// const note2 = new Tone.Loop((time) => {
+//   synth.triggerAttackRelease("a2", "8n", time);
+// }, "2n").start("4n");
 
-let loopIO = () => {
-  console.log("loop pressed");
+// const loopIO = () => {
+//   console.log("loop pressed");
 
-  currentvalue = document.getElementById("loopButton").value;
+//   currentvalue = document.getElementById("loopButton").value;
+//   if (currentvalue == "Off") {
+//     document.getElementById("loopButton").value = "On";
+//     Tone.Transport.start();
+//   } else {
+//     document.getElementById("loopButton").value = "Off";
+//     Tone.Transport.stop();
+//   }
+// }
+
+//test sequence function
+const seq1 = new Tone.Sequence(
+  (time, note) => {
+    synth.triggerAttackRelease(note, "16n", time);
+    // subdivisions are given as subarrays
+  },
+  ["C3", "D3", "E3", "F3", "G3", "A3", "B3", "C4"],
+).start(0);
+
+const seq2 = new Tone.Sequence(
+  (time, note) => {
+    synth.triggerAttackRelease(note, "16n", time);
+    // subdivisions are given as subarrays
+  },
+  ["C4", "D4", "E4", "F4", "G4", "A4", "B4", "C5"],
+).start("8n");
+
+const seqIO = () => {
+  console.log("sequence pressed");
+
+  currentvalue = document.getElementById("seqButton").value;
   if (currentvalue == "Off") {
-    document.getElementById("loopButton").value = "On";
+    document.getElementById("seqButton").value = "On";
     Tone.Transport.start();
   } else {
-    document.getElementById("loopButton").value = "Off";
+    document.getElementById("seqButton").value = "Off";
     Tone.Transport.stop();
   }
 }
