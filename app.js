1d6d7ed8f5618fb002ac6a0cad4b42bf5c608a3d
BLOS <blosguitarist@gmail.com>
2022-08-14 20:03:47 +0100

    updated sections

--- app.js
+++ app.js
@@ -1,11 +1,32 @@
-let startButton = document.getElementById("startButton");
-let tempoSlider = document.getElementById("tempoSlider");
-let bpmDisplay = document.getElementById("bpmDisplay");
+let startButton = document.getElementById("onoff-button");
+let tempoSlider = document.getElementById("tempo-slider");
+let bpmDisplay = document.getElementById("bpm-display");
+let decreaseTempo = document.getElementById("decrease-tempo");
+let increaseTempo = document.getElementById("increase-tempo");
+let beatsArray = [0, 1, 2, 3];
 
 bpmDisplay.innerHTML = tempoSlider.value;
 
+decreaseTempo.onclick = function () {
+  tempoSlider.value--;
+  bpmDisplay.innerHTML = this.value;
+  updateMetronome();
+};
+
+increaseTempo.onclick = function () {
+  tempoSlider.value++;
+  bpmDisplay.innerHTML = this.value;
+  updateMetronome();
+};
+
+
 tempoSlider.oninput = function () {
   bpmDisplay.innerHTML = this.value;
+  updateMetronome();
+};
+
+let updateMetronome = function () {
+  bpmDisplay.innerHTML = tempoSlider.value;
   Tone.Transport.bpm.value = tempoSlider.value;
 };
 
@@ -15,18 +36,18 @@ let loop = new Tone.Sequence(
     console.log(col);
     synth.triggerAttackRelease("C2", "64n");
   },
-  [0, 1, 2, 3, 4, 5, 6, 7],
+  beatsArray,
   "8n"
 ).start(0);
 
-let loop2 = new Tone.Sequence(
-  function (time, col) {
-    console.log(col);
-    synth.triggerAttackRelease("F2", "64n");
-  },
-  [0, 1, 2, 3, 4, 5, 6, 7],
-  "8n"
-).start("16n");
+// let loop2 = new Tone.Sequence(
+//   function (time, col) {
+//     console.log(col);
+//     synth.triggerAttackRelease("F2", "64n");
+//   },
+//   [0, 1, 2, 3, 4, 5, 6, 7],
+//   "8n"
+// ).start("16n");
 
 Tone.Transport.on("stop", function () {
   console.log("loop stopped");
